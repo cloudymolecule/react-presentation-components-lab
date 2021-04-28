6b71e29c3ced69d1acad9196838d286df4c50913
Cloudymolecule <baires82@gmail.com>
2021-04-28 12:53:39 -0400

    commit tree b936fa22ef9b6f3d2f094afc84d1a1d0624625d2

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => 'hello from index.js'} />
   </div>,
   document.getElementById('root')
 );
